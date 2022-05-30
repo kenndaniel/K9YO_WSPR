89f7f9a8248d6235e31372249d4df9fb3557e880
Kenneth Daniel <kenndaniel@yahoo.com>
2022-05-29 20:33:42 -0500

    Final flight 22-5

---
+++ src/CodeStandardMessage.h
@@ -0,0 +1,39 @@
+
+// ------ Message initialize functions -----
+
+
+#include "Sensors.h"
+
+
+
+
+
+
+void code_location()
+{
+
+  // calculate madenhead
+    POUTPUT((F(" Latitude =")));
+    POUTPUTLN((latitude, 6));
+    POUTPUT((F(" Longitude =")));
+    POUTPUTLN((longitude, 6));
+  double lon = longitude ;
+  double lat = latitude ;
+  //double lon = EEprom.getLongitude();
+  //double lat = EEprom.getLatitude();
+  char MH[9] = {'A', 'A', '0', '0', 'A', 'A', '0', '0', '\0'}; // Buffer for locator string
+  madenhead(lat, lon, MH);
+  charArrayCpy(loc4, MH, 4);
+  // POUTPUTLN((loc4));
+  charArrayCpy(loc6, MH, 6);
+  POUTPUT((F(" Grid Square =")));
+  POUTPUTLN((loc6));
+  charArrayCpy(loc8, MH[6], 2);
+}
+
+void code_standard_power()
+{
+  dbm_standard = codeAltitude(gpsAltitude);
+}
+
+
---
+++ src/CodeTelemetryMessage.h
@@ -0,0 +1,70 @@
+
+#include "CustomSensor.h"
+
+
+void code_characters(char Callsign[],  float gpsSpeed , float temp)
+{ // compose the custom telemtry callsign and convert values to characters and number
+        Callsign[0] = telemID[0]; // first part of telem call  e.g. T
+        Callsign[1] = telemID[1]; // second part of telem call e.g. 1
+        // The remainder of this message can be customized for different sensors
+        int fineSpeed = int(gpsSpeed) % 8;
+        Callsign[2] = codeNumberField(0, 9, fineSpeed);  // third position must be a number
+        Callsign[3] = codeCharacterField(0, 200, int(gpsSpeed));
+        
+        Callsign[4] = loc6[4];  // Example 5th character of grid square
+        Callsign[5] = loc6[5];  // Example 6th character of grid square
+        Callsign[6] = '\0';
+        Serial.println(Callsign);
+
+}
+
+void code_std_telem_characters(char Callsign[],  float volts , float temp, int sats)
+{ // compose the standard WB8ELK telemtry callsign -- convert values to characters and numbers
+        Callsign[0] = std_telemID[0]; // first part of telem call e.g. Q
+        Callsign[1] = codeStdPosition2(int(temp),sats);
+        Callsign[2] = std_telemID[1]; // second part of telem call e.g. 1
+        // Standard WB8ELK uses 3.3v to 5.8v for coding.  This is too large for a 3v system so 1v is added
+        Callsign[3] = codeCharacterField(33, 58, int((volts/100.+10.))); 
+        Callsign[4] = loc6[4];  // 5th character of grid square
+        Callsign[5] = loc6[5];  // 6th character of grid square
+        Callsign[6] = '\0';
+
+}
+
+void code_standard_telemetry_callsign()
+{
+  float tempCPU = getTempCPU();
+  float volts = readVcc();
+  // code telemetry callsign
+  code_std_telem_characters(call_telemetry, volts, tempCPU, satellites);
+  
+}
+
+void code_custom_telemetry_callsign()
+{
+  float tempCPU = getTempCPU();
+
+  // code telemetry callsign
+  code_characters(call_telemetry, gpsSpeed, tempCPU);
+  
+}
+
+void code_telemety_loc()
+{  // loc4_telemetry is coded into the 4 characters of the location field e.g. EN62
+  // This can be changed to code sensor data.
+  // The letters can take the value A-R the numbers 0-9
+    charArrayCpy(loc4_telemetry, loc4, 4);
+}
+
+
+void code_telemetry_power()
+{  // Altitude coded into the power field of the telemetry field
+  
+  dbm_telemetry = codeFineAltitude(gpsAltitude);
+}
+
+void code_custom_telemetry_power()
+{  // Altitude 0-60 is coded into the power field of the telemetry field
+
+  dbm_telemetry = codeFineFineAltitude(gpsAltitude);
+}
